4b13ed1d9005e8bd14c9094f50b17e75bcfcc5a1
Dillon Mendoza <mudd.network@gmail.com>
2026-06-11 09:47:43 -0600

    Configured views.py to include the new dashboard view and updated the URL patterns accordingly.

--- dashboard/dash/views.py
+++ dashboard/dash/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -15,3 +15,16 @@ def post_detail(request, pk):
 def post_new(request):
     form = PostForm()
     return render(request, 'dash/post_edit.html', {'form':form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'dash/post_edit.html', {'form': form})
