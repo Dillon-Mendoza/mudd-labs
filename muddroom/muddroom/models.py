91a1155d2896d85191f970a704f5231ba662485a
Dillon Mendoza <mudd.network@gmail.com>
2026-06-14 09:27:26 -0600

    Reconfigured the dashboard database to hold the service cards. Initialized hub.html to show new layout. Built the `Service` class in models.py then registered it through admin.py.

--- muddroom/muddroom/models.py
+++ muddroom/muddroom/models.py
@@ -2,3 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class Service(models.Model):
+
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+    description = models.CharField(max_length=200)
+    icon = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
--- muddroom/muddroom/views.py
+++ muddroom/muddroom/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Service
 from django.contrib.auth.decorators import login_required
 
 def hub(request):
-    return render(request, 'muddroom/hub.html')
+    services = Service.objects.filter(is_active=True)
+    return render(request, 'muddroom/hub.html', {'services': services})
